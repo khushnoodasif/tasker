6a869573866ef6368dab803d791a1812aff74042
Khushnood Asif <khushnood99@gmail.com>
2021-11-25 01:22:49 +0000

    ui fixes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const currentYear = new Date().getFullYear();
+
+function Note() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="note">
+      <h1>This is the note title</h1>
+      <p>This is the note content</p>
     </div>
   );
 }
 
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+        <h1 className="App-header">Tasker</h1>
+      </header>
+<div>
+  <div><Note /></div>
+</div>
+      <footer>
+        <p>Created By Khushnood Asif</p>
+        <p>Copyright &copy; {currentYear}</p>
+      </footer>
+    </div>
+    
+  );
+}
+
+
 export default App;
+ 
