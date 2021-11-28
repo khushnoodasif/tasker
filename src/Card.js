98cd05cfe6ae0161c2c4beb59120a9465ef65714
Khushnood Asif <khushnood99@gmail.com>
2021-11-28 22:37:36 +0000

    avatar component added

--- src/Card.js
+++ src/Card.js
@@ -1,11 +1,12 @@
 import react from 'react';
+import Avatar from './Avatar.js';
 
 function Card(props) {
   return (
     <div className="card">
       <div className="top">
         <h2 className="name">{props.name}</h2>
-        <img className="circle-img" src={props.img} alt="avatar_img" />
+        <Avatar img={props.img}/>>
       </div>
       <div className="bottom">
         <p className="info">{props.mobile}</p>
