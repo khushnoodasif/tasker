61f18ea4746e0d46266caca587409072c12f964d
Khushnood Asif <khushnood99@gmail.com>
2021-12-29 02:38:44 +0000

    Updates

---
+++ precache-manifest.82c9448b0e823a4ad1fa71e865b05ec3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfc17a84710195a56405b3fc6567ac62",
+    "url": "/tasker/index.html"
+  },
+  {
+    "revision": "e1760b7c97a70c6e3e94",
+    "url": "/tasker/static/css/main.d3a0dac9.chunk.css"
+  },
+  {
+    "revision": "dd5b45aeb8e88d36470a",
+    "url": "/tasker/static/js/2.e3dec199.chunk.js"
+  },
+  {
+    "revision": "e1760b7c97a70c6e3e94",
+    "url": "/tasker/static/js/main.6e8e98a0.chunk.js"
+  },
+  {
+    "revision": "0092cb97b92548af04c8",
+    "url": "/tasker/static/js/runtime-main.cf03f99c.js"
+  },
+  {
+    "revision": "f4934fb26707f41e88c183426ddb5747",
+    "url": "/tasker/static/media/logo.f4934fb2.svg"
+  }
+]);
